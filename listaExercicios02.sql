4e226b08a3084e80808935beea1c09c0a7721dcd
AnnyElly <143121386+AnnyElly@users.noreply.github.com>
2023-09-26 21:29:22 -0300

    listaExercicios02.sql
    exer.2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,3 +7,17 @@ DELIMITER ;
 
 
 CALL sp_ListarAutores();
+
+exer.2
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+
+
+CALL sp_LivrosPorCategoria('Romance');
