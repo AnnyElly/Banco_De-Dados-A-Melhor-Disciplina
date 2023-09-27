ce7a4f85b7f423a88b06865c6097a9aa06426aca
AnnyElly <143121386+AnnyElly@users.noreply.github.com>
2023-09-26 21:30:26 -0300

    Update listaExercicios02.sql
    exer.3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -21,3 +21,17 @@ DELIMITER ;
 
 
 CALL sp_LivrosPorCategoria('Romance');
+
+exer.3
+DELIMITER //
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT COUNT(Livro.Titulo) AS TotalLivros
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+
+
+CALL sp_ContarLivrosPorCategoria('Ciência');
